6e84df6153c22b13d5e331596989d80510daa2e7
Stefan Huynh <huynh.stefan@gmail.com>
2015-05-27 21:45:53 -0400

    Add update method to UsersController

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::UsersController < ApplicationController
+  before_action :require_logged_in, except: :create
+
   def create
     @user = User.new(user_params)
 
@@ -10,9 +12,20 @@ class Api::UsersController < ApplicationController
     end
   end
 
+  def update
+    @user = User.find(current_session.user_id)
+
+    if @user.update(user_params)
+      render :show, status: :ok
+    else
+      render json: { errors: @user.errors.full_messages },
+        status: :unprocessable_entity
+    end
+  end
+
   private
 
   def user_params
-    params.require(:user).permit(:username, :password)
+    params.require(:user).permit(:username, :password, :wins, :losses, :draws)
   end
 end
